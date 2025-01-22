2e9428ceb26bcafe30e1961c98d9dc54dc5090e9
Abhinav MS <abhinavms20002@gmail.com>
2025-01-22 21:50:47 +0530

    Other aditional changes

--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const productRouter = express.Router()
 const Auth =require("../middleware/userAuth")
+const roleMiddleware = require("../middleware/roleMiddleware");
 const {createProduct, 
     getProducts, 
     getProductForApproval,
@@ -12,9 +13,13 @@ const upload = require('../middleware/uploadImage')
 
 productRouter.post('/addproduct',Auth,upload.array("images",2),createProduct)
 productRouter.get('/getproduct',getProducts)
-productRouter.get('/getproductapproval',getProductForApproval)
-productRouter.get('/getproductforseller',Auth,getProductsforseller)
-productRouter.post('/approveproduct',approveProduct)
+productRouter.get('/getproductforseller',Auth,roleMiddleware("seller"),Auth,getProductsforseller)
+
+//admin side routes
+productRouter.get('/getproductapproval',Auth,roleMiddleware("admin"),getProductForApproval)
+productRouter.post('/approveproduct',Auth,roleMiddleware("admin"),approveProduct)
 productRouter.post('/updateproductprice',upadateProductprice)
+
+
 
 module.exports=productRouter;
